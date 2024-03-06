7b3e5e88bd6db79ea2a08d955dc289b3c3a1f82b
tazmattar <tazmattar@gmail.com>
2024-03-06 17:11:26 +0000

    new about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- src/routes/+page.svelte -->
+<div class="home_hero">
+    <h1>Enjoy Static Site Hosting With Kinsta StSH.</h1>
+    <p>Fast, Secure, Reliable Hosting Solution.</p>
+    <a href="https://kinsta.com/static-site-hosting/">
+        <div class="btn">Read more</div>
+    </a>
+</div>
+
+<style>
+    .home_hero {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        text-align: center;
+    }
+    .home_hero h1 {
+        font-size: 60px;
+        width: 70%;
+    }
+    .home_hero p {
+        color: #6e7076;
+        font-size: 20px;
+    }
+    .btn {
+        background-color: #5333ed;
+        padding: 20px 30px;
+        margin-top: 40px;
+        border-radius: 5px;
+        color: #fff;
+    }
+    @media (max-width: 700px) {
+        .home_hero h1 {
+            font-size: 40px;
+        }
+        .home_hero p {
+            font-size: 16px;
+        }
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,65 @@
+<div class="about_cont">
+    <h2>About</h2>
+    <div class="about_info">
+        <div class="about_text">
+            <p>
+                Kinsta allows you to{' '}
+                <a> href="https://kinsta.com/static-site-hosting/">
+                    host up to 100 static websites
+                </a>{' '}
+                for free. This can be done by pushing your code to your preferred Git provider
+                (Bitbucket, GitHub, or GitLab) and then deploying it to Kinsta.
+            </p>
+            <p>
+                As an alternative to Static Site Hosting, you can opt for deploying your
+                static site with Kinsta’s{' '}
+                <a> href="https://kinsta.com/application-hosting/">
+                    Application Hosting
+                </a>
+                , which provides greater hosting flexibility, a wider range of benefits,
+                and access to more robust features. For example, scalability, customized
+                deployment using a Dockerfile, and comprehensive analytics encompassing real-time
+                and historical data.
+            </p>
+        </div>
+        <iframe> width="560" height="315" src="https://www.youtube.com/embed/H7CNbsda8OA?si=40FGVlNvJ74_6hlj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen> </iframe>
+    </div>
+</div>
+
+<style>>
+    .about_cont h2 {
+        font-size: 40px;
+        margin-bottom: 20px;
+    }
+    .about_info {
+        display: flex;
+        width: 100%;
+        justify-content: space-between;
+        gap: 20px;
+    }
+    @media (max-width: 700px) {
+        .about_info {
+            flex-direction: column;
+        }
+    }
+    .about_text {
+        flex-basis: 50%;
+    }
+    .about_text p {
+        margin-bottom: 20px;
+        font-size: 18px;
+    }
+    .about_img {
+        flex-basis: 50%;
+        width: 200px;
+        border-radius: 10px;
+    }
+    @media (max-width: 700px) {
+        .about_info {
+            flex-direction: column;
+        }
+        .about_img {
+            width: 100%;
+        }
+    }
+</style>
